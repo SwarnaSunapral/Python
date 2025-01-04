37ec60d96e2e34cce154f8ee965d87ad7b7b93f2
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-04 12:55:52 +0530

    Update operators.py

--- stage1/operators.py
+++ stage1/operators.py
@@ -1,1 +1,80 @@
+# Arithmetic Operators
+
+a = 5
+b = 2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a % b)  # remainder is the answer as we are using modulo operator
+print(a ** b) # a ^ b
+
+#............................................................
+
+#Relational Operators
+
+a = 5
+b = 4
+
+print(a == b) # False
+print(a != b) # True
+print(a > b)  # True
+print(a < b)  # False
+print(a <= b) # False
+print(a >= b) # True
+
+#..............................................................
+#Assignment Operator
+
+num = 20
+num += 10
+print(num)  # 20 + 10 = 30
+
+num -= 10
+print(num) # 30 - 10 = 20
+
+num *= 2
+print(num) # 20 * 2 = 40
+
+num /= 2 
+print(num) # 40/2 = 20.0
+
+num **= 2
+print(sum) # 20.0 ^ 2 = 400.0
+
+#............................................................
+#Logical Operator
+# not operator
+a = 5
+b = 4
+print(not False)   # True
+print(not True)    # False
+print(not (a > b)) # False
+
+a = 5
+b = 8
+print(not (a > b)) # True
+
+# and operator 
+a = True
+b = True 
+print(a and b) #True
+
+a = True
+b = False
+print(a and b) #False
+# if atleast on eof the value is false then it gives false
+
+# OR Operator
+a = True
+b = False
+print(a or b) #True
+# if atleast one of the operator is true then it gives true as one of the value is true.
+
+
+
+
+
+
 
