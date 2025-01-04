051ada4f6274f7a694908d64fbc1fd1ea557e7d7
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-04 13:33:55 +0530

    Update input.py

--- stage1/input.py
+++ stage1/input.py
@@ -1,1 +1,5 @@
+input("Enter the name:")        # When you run the statement it asks to enter the name. As soon as you press enter, then the program finishes.
+
+name = input("Enter the name:") # we can store the value in the variable
+print("welcome", name)          # prints welcome Swarna after taking the value from the user
 
