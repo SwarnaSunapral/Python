6d46dfad69140aab41d1ba31aec2e2d97f5ee40f
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-04 14:44:02 +0530

    Update practiceQuestions.py

--- stage1/practiceQuestions.py
+++ stage1/practiceQuestions.py
@@ -11,3 +11,11 @@ side = int(input("Enter the side of a square:"))  # side = 5
 area = side*side                                  # area = 25
 print("Area of a square is:", area)  
 
+#3. WAP to input 2 floating point numbers & print their average
+
+val1 = float(input("Enter the value:"))         # 3.0
+val2 = float(input("Enter the value:"))         # 5.0
+sum = val1 + val2                               # 8.0
+average = sum/2                                 # 4.0
+print(sum)
+print("Average of the numbers is:", average)
