5aac5c791d38a364de2472c62b3d5578a3866625
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-04 14:26:18 +0530

    Update input.py

--- stage1/input.py
+++ stage1/input.py
@@ -3,3 +3,10 @@ input("Enter the name:")        # When you run the statement it asks to enter th
 name = input("Enter the name:") # we can store the value in the variable
 print("welcome", name)          # prints welcome Swarna after taking the value from the user
 
+#..............................................
+val = int(input("Enter the value")
+print(type(val), val)           # It takes it as integer here as we kept the datatype int before input()
+
+
+          
+
