a520eb3fc04bb4bd1e094c34ed75402ad5b4723d
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-04 14:39:01 +0530

    Update practiceQuestions.py

--- stage1/practiceQuestions.py
+++ stage1/practiceQuestions.py
@@ -1,6 +1,13 @@
-# Write a program to input 2 numbers & print their sum.
+# 1. Write a program to input 2 numbers & print their sum.
 
-a = int(input("Enter the value of a:"))
-b = int(input("Enter the value of b:"))
-sum = a + b
-print(type(sum), sum)
+a = int(input("Enter the value of a:"))  # a = 5
+b = int(input("Enter the value of b:"))  # b = 2
+sum = a + b                              # sum = 7
+print(type(sum), sum)               
+
+#2. WAP to input side of a square & print its area
+
+side = int(input("Enter the side of a square:"))  # side = 5
+area = side*side                                  # area = 25
+print("Area of a square is:", area)  
+
