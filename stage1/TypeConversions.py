2873037c5fd3deb45fea030198e10be75138b6dc
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-04 13:19:40 +0530

    Update TypeConversions.py

--- stage1/TypeConversions.py
+++ stage1/TypeConversions.py
@@ -1,1 +1,15 @@
+# Type Conversions
+
+a = 10
+b = 2.5
+sum = a + b # a is automatically coverted to float. Because float is the superior value.
+print(sum)
+
+# Type Casting
+a = "10" 
+b = 2.5
+sum = int(a) + b # a is type casted manually 
+print(sum)
+print(type(a))
+
 
