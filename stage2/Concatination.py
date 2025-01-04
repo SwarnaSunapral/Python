54b4c8b6be90c90b73be13aa59593aa8f18d152b
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-04 18:29:11 +0530

    Update and rename Strings.py to Concatination.py

---
+++ stage2/Concatination.py
@@ -0,0 +1,8 @@
+# Concatenation
+str1 = "Hello"
+str2 = "world"
+str3 = str1 + str2                # Helloworld
+print(str3)  
+print(len(str3))                  # 10
+print(len(str1 + "   " + str2))   # 13
+
--- stage2/Strings.py
+++
@@ -1,5 +0,0 @@
-# Concatenation
-str1 = "Hello"
-str2 = "world"
-str3 = str1 + str2  # Helloworld
-print(str3)       
