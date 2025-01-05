19a922b090db5c1143fef773f69e241368291ce7
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-05 12:06:29 +0530

    Update Conditional_statements.py

--- stage2/Conditional_statements.py
+++ stage2/Conditional_statements.py
@@ -1,1 +1,29 @@
+light = "green"
 
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):               # code doesnot check this condition because the statement is true in the previous statement itself. so it stops checking the furthur lines
+    print("look")
+print("End od code")
+
+#...............................................................................................................................
+
+num = 6
+
+if(num > 2):
+    print("yes")                      # yes
+if(num > 3):
+    print("yes")                      # yes
+
+#Because here there are two if conditions are present 
+
+num = 6
+
+if(num > 2):
+    print("yes")                     # yes
+elif(num > 3):
+    print("yes")
+
+# here the output is only one yes because when the if condition is true it doesnot check the next condition 
