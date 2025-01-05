e56aa336523730e276bbf446e741fc7146792336
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-05 11:19:18 +0530

    Update Practice.py

--- stage2/Practice.py
+++ stage2/Practice.py
@@ -1,1 +1,12 @@
+#1. WAP to input user's first name & print its length
+
+str = input("Enter the first name:")   # Swarna
+print(len(str))                        # 6
+
+#2. WAP to find the occurrence of'$' in a string
+
+str = "I am $warna from $rinagar and i can $ing better"      
+print(str.count("$"))                  # 3
+
+
 
