b0dc5ffa78c7c8ccc7089da0c0e74e8d68201567
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-05 12:07:33 +0530

    Update Practice.py

--- stage2/Practice.py
+++ stage2/Practice.py
@@ -9,4 +9,17 @@ str = "I am $warna from $rinagar and i can $ing better"
 print(str.count("$"))                  # 3
 
 
+#3. To print the grades of the students according to the marks they obtain
+
+marks = int(input("Enter the marks:"))
+
+if(marks >= 90):
+    print("grade = A")
+elif(marks >= 80 and marks < 90):
+    print("grade = B")
+elif(marks >= 70 and marks < 80):
+    print("grade = C")
+elif(marks < 70):
+    print("grade = D")
+
 
