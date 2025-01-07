1b9b82293704cb1bd6905ac879e722bd93fced74
Swarnalatha Bai Sunapral <122410159+SwarnaSunapral@users.noreply.github.com>
2025-01-07 06:11:35 +0530

    Update Lists.py

--- Stage3/Lists.py
+++ Stage3/Lists.py
@@ -1,1 +1,24 @@
+# Lists
+
+marks1 = 94.4
+marks2 = 87.5
+marks3 = 95.2
+marks4 = 66.4
+marks5 = 45.1
+
+print(marks)
+print(len(marks))
+print(marks[0])
+print((marks[4]))
+print(marks[5]))         # Here the output is error becaus the index 5 doesnot exists. List should access the data within the list.
+
+
+
+
+
+
+
+
+
+
 
